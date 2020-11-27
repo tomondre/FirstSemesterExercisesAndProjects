db4edc891aee4ea8f7d89970dcbbbb90342c96d2
tomas-ondrejka <tomas.ondrejka2000@gmail.com>
2020-11-27 13:41:53 +0100

    Initial commit

---
+++ Workshop/src/Champion/ChampionList.java
@@ -0,0 +1,14 @@
+package Champion;
+
+public class ChampionList
+{
+  private int size;
+  public ChampionList(int size)
+  {
+    this.size=size;
+  }
+  public void addChampion(Champion champion)
+  public void removeChampion(ch)
+
+
+}
